58c1d3a44146ccd0930f557448e2ca60772377a8
Noori Fatima <noorifatima@Akthars-MacBook-Air.local>
2022-03-31 21:53:16 +0530

    mobile page is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import Home from './components/Home/Home'
-import Login from './components/common/login/Login';
-import PhoneLogin from './components/common/login/PhoneLogin';
-import SignUp from './components/common/signup/SignUp'
+import React from "react";
+// import Home from "./components/Home/Home";
+// import Login from "./components/common/login/Login";
+// import PhoneLogin from "./components/common/login/PhoneLogin";
+// import SignUp from "./components/common/signup/SignUp";
+import MobilePage from "./components/MobilePage/MobilePage";
 
 const App = () => {
-  
-  return(<div>
-    {/* <Login/> */}
-    {/* <PhoneLogin/> */}
-    <SignUp/>
-  
-  </div>)
-  
+  return (
+    <div>
+      {/* <Login/> */}
+      {/* <PhoneLogin/> */}
+      {/* <SignUp/> */}
+      <MobilePage />
+    </div>
+  );
 };
 
 export default App;
