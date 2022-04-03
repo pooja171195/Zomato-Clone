48e1db5887c714986a39f5c646bb75d5693d29b4
pooja yadav <96077451+pooja171195@users.noreply.github.com>
2022-04-03 09:32:51 +0530

    * added

--- frontend/src/components/PageNotFound/PageNotFound.jsx
+++ frontend/src/components/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./PageNotFound.css";
+import Header from '../header/header';
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <>
+    <div>
+      <Header/>
+    </div>
+    <hr/>
+    <div className='PImg'>
+<img src='https://b.zmtcdn.com/404_web787058f236e16cef33bf733ade2e08ba1574663725.png?output-format=webp' alt=''/>
+    </div>
+    <div className='PBtn'>
+      <h3>We can't seem to find the page you're looking for...</h3>
+<button onClick={'/'}>Back to home</button>
+    </div>
+    </>
   )
 }
 
